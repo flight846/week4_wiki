907973a426adff17cb02b174939f83a06f4bea1a
yazid <flight846@gmail.com>
2016-06-24 00:05:19 +0800

    Add static controller & add static routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'static#about'
+    
+  get '/about', to: 'static#about'
+
+  get 'faq', to: 'static#faq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
