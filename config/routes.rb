bf15e9e4e23d768c7b12c47ba282c8c87d6e5ec4
yazid <flight846@gmail.com>
2016-06-24 13:06:14 +0800

    Add styles to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  resources :scaffolds
+  get 'git/status'
+
   resources :articles
   resources :pages
   root 'static#about'
     
   get '/about', to: 'static#about'
 
-  get 'faq', to: 'static#faq'
+  get '/faq', to: 'static#faq'
+  
+  get "articles/page"
+  
+  get "/aeroplane", to: 'aeroplane#index'
+    
+  root 'static#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
